642700baf649ce2be683e2b045bcc2daf115afd5
mtabdev <84842990+mtabdev@users.noreply.github.com>
2022-07-05 02:32:14 +0530

    Time: 2 ms (87.28%), Space: 57.3 MB (6.86%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+          int c=0;
+        int ele=0;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            if(c==0){
+                ele=nums[i];
+            }
+            if(nums[i]==ele)
+                c++;
+            else
+                c--;
+        }
+        
+        return ele;
+    }
+}
