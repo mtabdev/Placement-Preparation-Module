99d44b225d67e6414d4ee9200f6d5555dce54e47
mtabdev <84842990+mtabdev@users.noreply.github.com>
2022-07-06 21:45:03 +0530

    Time: 119 ms (82.03%), Space: 6 MB (93.13%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int mi=INT_MAX;
+           for(int j=1;j*j<=i;j++){
+               int rem=i-(j*j);
+               if(dp[rem]<mi)mi=dp[rem];
+               
+           }
+            dp[i]=mi+1;
+            //cout<<dp[i]<<" ";
+        }
+        return dp[n];
+    }
+};
